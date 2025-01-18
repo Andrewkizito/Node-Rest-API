b6ce3dbde80811fa59ce26fff872fd4e96199472
andrewkizito <andrewkizito54@gmail.com>
2025-01-18 11:36:28 +0300

    feat: simple server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Server Running");
+import { createServer } from "node:http";
+import os from "node:os";
+
+const app = createServer((req, res) => {
+  console.log(JSON.stringify(req));
+  res.writeHead(200, {
+    "content-type": "Appliction/json",
+    server: `${os.arch()} ${os.hostname} ${os.machine()} ${os.version} ${
+      os.platform
+    }`,
+  });
+
+  res.end(JSON.stringify({ app: "Hello" }));
+});
+
+app.listen(8000, () => console.log("Server started on http://localhost:8000"));
